7027366662606230d1c71ad96f32817dbd024e10
James Webb <jbwebb@jamess-mbp.dyndns.rice.edu>
2019-06-27 10:15:14 -0500

    added serial data stream

--- capacitiveSensor/python/get_serial.py
+++ capacitiveSensor/python/get_serial.py
@@ -1,6 +1,8 @@
 import serial
 import sys
 import argparse
+import math
+import time
 
 BAUD_RATES = [300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 28800, 38400, 57600, 115200]
 
@@ -11,8 +13,10 @@ parser.add_argument('-p', '--port', type=str, default='/dev/ttyACM0',
                     help='port on which Arduino is mounted')
 parser.add_argument('-b', '--baud-rate', type=int, choices=BAUD_RATES, default=9600,
                     help='baud rate to read serial data')
-#parser.add_argument('-s', '--size', type=int, default=1,
-#                    help='byte size of messages')
+parser.add_argument('-t', '--timeout', type=float, default=0,
+                    help='serial port timeout (s)')
+parser.add_argument('-d', '--duration', type=float, default=None,
+                    help='duration of data acquisition (s)')
 args = parser.parse_args()
 
 with open(args.output_filepath, 'wb') as f:
@@ -23,10 +27,21 @@ with open(args.output_filepath, 'wb') as f:
                         bytesize=serial.EIGHTBITS,
                         timeout=1)
     input('Press any key to start...')
-    print('Piping from %s to %s...')
-    while ser.is_open:
-        f.write(ser.readline())
+    print('Piping from %s to %s' % (args.port, args.output_filepath))
+    
+    if args.duration is not None:
+        t_total = args.duration
+    else:
+        t_total = math.inf
+        print('Press Ctrl-C to stop.')
+
+    t_start = time.time()
+    while ser.is_open and ((time.time() - t_start) < t_total):
+        if ser.in_waiting > 0:
+            f.write(ser.readline())
     ser.close()
+
+print('Logged %.2f seconds of data.' % t_total)
 
 
 
