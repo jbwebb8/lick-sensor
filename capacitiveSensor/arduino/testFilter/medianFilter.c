a74112c4838440460e172a5bcd9cbc14de93a924
jbwebb <jbwebb@bcm.edu>
2019-07-15 16:58:52 -0500

    renamed files; fixed median filter bugs

--- capacitiveSensor/arduino/testFilter/medianFilter.c
+++ capacitiveSensor/arduino/testFilter/medianFilter.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
 
 // Get random integer in [lower, upper]
 // Credit: https://www.geeksforgeeks.org/generating-random-number-range-c/
@@ -68,7 +70,8 @@ long quickselect(long arr[], int k, int left, int right) {
     }
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    /*
     // NOTE: In C, arrays are passed as pointers, so size information is lost.
     // If another function needs array size information, it must be passed as
     // a separate parameter.
@@ -87,12 +90,35 @@ int main(void) {
     else {
         k = (int) sizeof(arr) / sizeof(arr[0]) / 2;
     }
+    */
 
-    // Perform quickselect algorithm
-    long median = quickselect(arr, k, 0, N-1);
+    // Get command line arguments
+    int N = atoi(argv[1]); // length of array
+    int k = atoi(argv[2]); // k-th median
+    int M = atoi(argv[3]); // number of iterations
 
-    // Print result
-    printf("%d th element of array is %ld.\n", k, median);
+    // Create random array
+    long arr[N];
+    srand(time(NULL));
+    for (int n = 0; n < N; n++) {
+        arr[n] = rand();
+    }
 
+    // Time loops
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+    for (int m = 0; m < M; m++) {
+        // Perform quickselect algorithm
+        long median = quickselect(arr, k, 0, N-1);
+
+        // Print result
+        //printf("%d th element of array is %ld.\n", k, median);
+    }
+    gettimeofday(&end, NULL);
+    double tTotal = ((double) (end.tv_sec - start.tv_sec)) + 10e-6*((double) (end.tv_usec - start.tv_usec));
+    double tLoop = tTotal / (double) M;
+    printf("Total time: %lf s (%lf s per loop)\n", tTotal, tLoop);
+
+    // Exit
     return(0);
 }
